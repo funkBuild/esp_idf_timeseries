5661d85c7643a805431d7ff2b51cbedb4500ea8d
Matt Anderson <manderson8787@gmail.com>
2026-03-11 11:14:11 +0800

    Improvements to memory usage and queries

--- test_app/main/hello_world_main.c
+++ test_app/main/hello_world_main.c
@@ -1,8 +1,22 @@
 #include "unity.h"
+#include "esp_heap_caps.h"
+#include "esp_log.h"
+#include <inttypes.h>
+
+void setUp(void) {
+  ESP_LOGI("HEAP", "%" PRIu32 " free, %" PRIu32 " largest",
+           (uint32_t)heap_caps_get_free_size(MALLOC_CAP_DEFAULT),
+           (uint32_t)heap_caps_get_largest_free_block(MALLOC_CAP_DEFAULT));
+}
+
+void tearDown(void) {}
 
 void app_main(void) {
+  ESP_LOGI("test_main", "Free heap at start: %" PRIu32 " bytes, largest block: %" PRIu32 " bytes",
+           (uint32_t)heap_caps_get_free_size(MALLOC_CAP_DEFAULT),
+           (uint32_t)heap_caps_get_largest_free_block(MALLOC_CAP_DEFAULT));
 
+  UNITY_BEGIN();
   unity_run_all_tests();
-
-  unity_run_menu();
+  UNITY_END();
 }
